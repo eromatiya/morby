7c8c9d6f0f8a10956395f7772a0065c24f51e6ce
Gerome Matilla <gerome.matilla07@gmail.com>
2022-06-19 11:51:55 +0800

    fix mistake: remove express

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
-const express = require('express')
-const app = express();
-const port = 3000
-
-app.get('/', (req, res) => res.send('All right!'))
-
-app.listen(port, () =>
-	console.log(`Your app is listening a http://localhost:${port}`)
-);
+require('dotenv').config();
 
 const { Client, Intents } = require('discord.js');
 
